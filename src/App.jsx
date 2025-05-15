e6d396f450a91aaa3e30f77e30e8ad1ed6a4248b
rubenaguilardev <ragui56@wgu.edu>
2025-05-14 22:41:24 -0700

    Add header, status section

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
+export default function App() {
 
+  return (
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+      <section className='status'>
+        <h2>You win!</h2>
+        <span>Well done! 🎉</span>
+      </section>
+    </main>
+  )
+}
